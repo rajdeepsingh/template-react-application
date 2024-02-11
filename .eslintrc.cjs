0629fffc1f96669812a9f8fc9889a32820244b36
Rajdeep Singh <rajdotdev+github@gmail.com>
2024-02-11 14:58:22 -0700

    Added some recommended eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,18 @@ module.exports = {
   env: { browser: true, es2021: true, node: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'prettier',
+    'plugin:@typescript-eslint/recommended',
+
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+
+    'plugin:import/recommended',
+    'plugin:tailwindcss/recommended',
   ],
   ignorePatterns: ['node_modules', 'dist', '.eslintrc.cjs', '.github'],
   parser: '@typescript-eslint/parser',
-  plugins: ['prettier', '@typescript-eslint', 'react-refresh'],
+  plugins: ['import', 'jsx-a11y', 'prettier', '@typescript-eslint', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
